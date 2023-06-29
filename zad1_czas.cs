f2411d05104d184b242270491d4409fc3243b786
Sebson1337 <116345984+Sebson1337@users.noreply.github.com>
2023-06-29 19:22:34 +0200

    Add files via upload

---
+++ zad1_czas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Podaj liczbę sekund: ");
+        int sekundy = Convert.ToInt32(Console.ReadLine());
+        
+        string sformatowanyCzas = FormatujCzas(sekundy);
+        Console.WriteLine(sformatowanyCzas);
+    }
+
+    static string FormatujCzas(int sekundy)
+    {
+        int godziny = sekundy / 3600;
+        int minuty = (sekundy % 3600) / 60;
+        int resztaSekund = sekundy % 60;
+
+        string sformatowaneGodziny = godziny.ToString("D2");
+        string sformatowaneMinuty = minuty.ToString("D2");
+        string sformatowaneSekundy = resztaSekund.ToString("D2");
+
+        string sformatowanyCzas = $"{sformatowaneGodziny}:{sformatowaneMinuty}:{sformatowaneSekundy}";
+        return sformatowanyCzas;
+    }
+}
---
+++ zad3_kartezjanski.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        double x1 = 3.5;
+        double y1 = 1.5;
+        double x2 = 2.5;
+        double y2 = 5.5;
+
+        double dlugoscOdcinka = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+        Console.WriteLine($"dlugosc odcinka: {dlugoscOdcinka}");
+    }
+
+    static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+    {
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+
+        double dlugosc = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        return dlugosc;
+    }
+}
